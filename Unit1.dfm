207d509bf0a1682fe99de172c00704962cb7722f
tokomonster <41684944+tokomonsters@users.noreply.github.com>
2018-08-15 01:55:36 +0800

    Add files via upload

---
+++ Unit1.dfm
@@ -0,0 +1,68 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 352
+  ClientWidth = 726
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 24
+    Top = 176
+    Width = 81
+    Height = 49
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object ocvView1: TocvView
+    Left = 48
+    Top = 24
+    Width = 305
+    Height = 113
+    Frames = <>
+  end
+  object Button2: TButton
+    Left = 111
+    Top = 176
+    Width = 75
+    Height = 49
+    Caption = 'Button2'
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 192
+    Top = 176
+    Width = 81
+    Height = 49
+    Caption = 'Button3'
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 32
+    Top = 272
+    Width = 81
+    Height = 41
+    Caption = #20108#20540#21270
+    TabOrder = 4
+    OnClick = Button4Click
+  end
+  object Button5: TButton
+    Left = 144
+    Top = 272
+    Width = 161
+    Height = 41
+    Caption = 'cvAdaptiveThreshold'
+    TabOrder = 5
+    OnClick = Button5Click
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,212 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs,ocv.highgui_c,
+  ocv.core_c,
+  ocv.core.types_c,
+  ocv.comp.types,
+  ocv.imgproc_c,
+  ocv.cls.core,
+  ocv.cls.highgui,
+  ocv.imgproc.types_c, ocv.comp.View, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    ocvView1: TocvView;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  lvV:TocvImage;
+  image: pIplImage ;
+begin
+  image:= cvLoadImage('C:\Test image\c02.jpg') ;
+
+
+
+
+ // lvV:=TocvImage.Create(cvLoadImage('C:\Test image\c02.jpg')) as TocvImage;
+ // ocvView1.DrawImage(lvV);
+
+
+ // cvReleaseImage(lvV);
+ // lvV:=TocvImage.Create(;
+ // lvV.LoadFormFile('C:\Test image\c02.jpg');
+
+  //lvV.IplImage :=  cvLoadImage('C:\Test image\c02.jpg');
+ // ocvView1.DrawImage(lvV);
+  //lvV.Free;
+  //ocvView1.DrawImage(TocvImage(cvLoadImage('C:\Test image\c02.jpg')));
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+Var
+  image: pIplImage ;
+  M: IMat;
+  ii:integer;
+begin
+  try
+    ReportMemoryLeaksOnShutdown := True;
+    for ii := 0 to 100 do begin
+      image := cvLoadImage('C:\Test image\c02.jpg');
+      M := TMat.Create(image);
+      M := nil;
+     // ocvView1.DrawImage(image);
+
+      showmessage('');
+      cvReleaseImage(image);
+    end;
+  except
+    on E: Exception do
+      showmessage(E.ClassName+ ': '+ E.Message);
+  end;
+  showmessage('ok');
+
+
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  Mat:IMat;
+  image: pIplImage ;
+  lvV:TocvImage;
+  BMP:TBitmap;
+begin
+  Mat := imread('C:\Test image\c02.jpg');
+  image := cvLoadImage('C:\Test image\c02.jpg');
+  lvV:=TocvImage.Create(image) as TocvImage;
+  BMP := lvV.AsBitmap;
+  if BMP<>nil then begin
+   BMP.SaveToFile('C:\Test image\c02.bmp');
+  end;
+  BMP.Free;
+  lvV.Free;
+  image := nil;
+ // ocvView1.DrawImage(lvV);
+
+
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+var
+  sor_th,dst_th: pIplImage;
+  lvV:TocvImage;
+   BMP:TBitmap;
+begin
+  sor_th := cvLoadImage('C:\Test image\r2.jpg',CV_LOAD_IMAGE_GRAYSCALE);
+  dst_th := cvCreateImage(cvGetSize(sor_th), IPL_DEPTH_8U, 1);
+ // dst_th := cvCreateImage(cvSize(sor_th.width,sor_th.height),sor_th.depth,sor_th.nChannels);
+ //以下示範threshold()的用法，將灰階值小於150的設為0，大於150的設為255：
+  //cvThreshold(sor_th, dst_th, 90, 255, CV_THRESH_BINARY);
+  cvAdaptiveThreshold(sor_th, dst_th, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 101, 0);
+  lvV:=TocvImage.Create(dst_th) as TocvImage;
+  ocvView1.DrawImage(lvV);
+  BMP :=  lvV.AsBitmap;
+  if BMP<>nil then begin
+   BMP.SaveToFile('C:\Test image\result.bmp');
+   BMP.Free;
+  end;
+    // grayImage = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);//1代表灰階圖
+ // cvReleaseImage(dst_th);
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+const
+  filename = 'C:\Test image\r2.jpg';
+  SaveFN = 'C:\Test image\Save.jpg';
+  SaveFN2 = 'C:\Test image\Save2.jpg';
+var
+  src: pIplImage ;
+  dst: pIplImage ;
+  dst2: pIplImage;
+  AcvRect:TcvRect;
+
+begin
+  try
+    // 瀁鋹欑樦 罻貗鴈膧
+    src := cvLoadImage(filename, CV_LOAD_IMAGE_GRAYSCALE);
+   // showmessage(Format('[i] image: %s', [filename]));
+    // 瀁罻緪?鳿鍕譇緪膻?    cvNamedWindow('original', CV_WINDOW_AUTOSIZE);
+    cvShowImage('original', src);
+    dst := cvCreateImage(cvSize(src^.width, src^.height), IPL_DEPTH_8U, 1);
+    dst2 := cvCreateImage(cvSize(src^.width, src^.height), IPL_DEPTH_8U, 1);
+
+    cvThreshold(src, dst, 150, 255, CV_THRESH_BINARY);
+    cvAdaptiveThreshold(src, dst2, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY, 155, 1);
+
+
+    cvNamedWindow('cvThreshold', CV_WINDOW_AUTOSIZE);
+    cvShowImage('cvThreshold', dst);
+   // cvSaveImage(SaveFN ,dst);
+
+
+    cvNamedWindow('cvAdaptiveThreshold', CV_WINDOW_AUTOSIZE);
+    cvShowImage('cvAdaptiveThreshold', dst2);
+       // cvSetImageROI(dst2,  cvRect(20,0,0,0) );
+        cvSetImageROI(dst2, ocv.core.types_c.cvRect(0,0,dst2.width,200));
+        cvSetZero(dst2);
+        cvResetImageROI(dst2);
+
+        cvSetImageROI(dst2, ocv.core.types_c.cvRect(0,370,dst2.width,dst2.height));
+        cvSetZero(dst2);
+        cvResetImageROI(dst2);
+
+
+
+
+        cvSaveImage(SaveFN ,dst2);
+        cvSetImageROI(dst, ocv.core.types_c.cvRect(0,0,dst.width,200));
+        cvSetZero(dst);
+        cvResetImageROI(dst);
+
+        cvSetImageROI(dst, ocv.core.types_c.cvRect(0,370,dst.width,dst2.height));
+        cvSetZero(dst);
+        cvResetImageROI(dst);
+
+
+
+
+        cvSaveImage(SaveFN2 ,dst);
+
+    // 緛賄 縺糈蠂 膹飶儑?
+   // cvWaitKey(0);
+
+    // 蟃碭搿緛馲?謥c鶇c?
+    cvReleaseImage(src);
+    cvReleaseImage(dst);
+    cvReleaseImage(dst2);
+    // 鵫鳪樦 鍧縺
+    cvDestroyAllWindows;
+  except
+    on E: Exception do
+      WriteLn(E.ClassName, ': ', E.Message);
+  end;
+
+end;
+
+end.
